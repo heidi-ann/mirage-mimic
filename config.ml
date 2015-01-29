860da36645ec80a21b07389867de3c7129d6c81c
heidi-ann <hh360@cam.ac.uk>
2015-01-29 11:21:50 +0000

    adding tls dependancy

--- config.ml
+++ config.ml
@@ -2,9 +2,14 @@ open Mirage
 
 let main = foreign "Unikernel.Main" (console @-> network @-> job)
 
+let platform =
+    match get_mode () with
+    | `Unix -> "unix"
+    | `Xen -> "xen"
+
 let () =
-  add_to_ocamlfind_libraries ["cstruct"; "cstruct.syntax"; "re"; "re.str"; "tcpip.ethif"; "tcpip.tcp"; "tcpip.udp"; "tcpip.stack-direct" ];
-  add_to_opam_packages ["cstruct"; "tcpip"; "re"];
+  add_to_ocamlfind_libraries ["cstruct"; "cstruct.syntax"; "re"; "re.str"; "tcpip.ethif"; "tcpip.tcp"; "tcpip.udp"; "tcpip.stack-direct";  "mirage-clock-" ^ platform; "tls" ];
+  add_to_opam_packages ["cstruct"; "tcpip"; "re"; "mirage-clock-" ^ platform; "tls"; "tls.mirage"];
   register "unikernel" [
     main $ default_console $ tap0
   ]
