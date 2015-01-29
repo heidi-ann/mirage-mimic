543b6a86dfdf6ae7116a8bf8b16c7e7cda42c87f
heidi-ann <hh360@cam.ac.uk>
2015-01-29 16:26:05 +0000

    removing exn

--- unikernel.ml
+++ unikernel.ml
@@ -200,6 +200,6 @@ module Main (C: V1_LWT.CONSOLE) (Netif : V1_LWT.NETWORK) (E : ENTROPY) (KV : KV_
                     Stack.listen s
               end
     | `TLS -> begin
-                    TLSS.start c s e kv (List.hd_exn dest_ports)
+                    TLSS.start c s e kv (List.hd dest_ports)
               end
 end
